d68631103e115ebbb7fbc39a91ddb4df7265fb5f
Roko Krstulović <roko.krstulovic.opara@gmail.com>
2021-06-02 21:16:17 +0200

    generat empty components

---
+++ reactjs/src/AboutMe/AboutMe.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './AboutMe.module.scss';
+
+const AboutMe: React.FC = () => (
+  <div className={styles.AboutMe}>
+    AboutMe Component
+  </div>
+);
+
+export default AboutMe;
---
+++ reactjs/src/Skills/Skills.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Skills.module.scss';
+
+const Skills: React.FC = () => (
+  <div className={styles.Skills}>
+    Skills Component
+  </div>
+);
+
+export default Skills;

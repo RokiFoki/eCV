3237323a1da6df3a99847249d5a7262746d5a9e5
Roko Krstulović <roko.krstulovic.opara@gmail.com>
2021-06-08 18:19:10 +0200

    highlight correct route menu item

--- reactjs/src/App.tsx
+++ reactjs/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Layout, Menu, } from 'antd';
 import { UserOutlined, PhoneFilled } from '@ant-design/icons';
-import { BrowserRouter as Router, Link, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Redirect, Route, Switch, useLocation } from 'react-router-dom';
 
 import './App.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -13,7 +13,40 @@ import Contact from './Contact/Contact.lazy';
 
 const { Sider } = Layout;
 
-const App = () => (
+const SideNavbarContent = () => {
+  const path = useLocation().pathname;
+  let selectionKey = '1';
+  if (path.startsWith('/experience'))  {
+    selectionKey = '2';
+  } else if (path.startsWith('/skills')) {
+    selectionKey = '3';
+  } else if (path.startsWith('/contact')) {
+    selectionKey = '4';
+  }
+  return (
+    <React.Fragment>
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={[selectionKey]}>
+        <Menu.Item key="1" icon={<UserOutlined />}>
+          <Link to="/">About me</Link>
+        </Menu.Item>
+        <Menu.Item key="2" icon={<FontAwesomeIcon icon={faBook} />}>
+          <Link to="/experience">Experience</Link>
+        </Menu.Item>
+        <Menu.Item key="3" icon={<FontAwesomeIcon icon={faChess} />}>
+          <Link to="/skills">Skills</Link>
+        </Menu.Item>
+        <Menu.Item key="4" icon={<PhoneFilled />}>
+          <Link to="/contact">Contact</Link>
+        </Menu.Item>
+      </Menu>
+    </React.Fragment>
+  )
+}
+
+
+const App = () => {
+  return (
   <Router>
     <Layout>
       <Sider id="components-layout-demo-fixed-sider"
@@ -24,21 +57,7 @@ const App = () => (
           left: 0,
         }}
       >
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            <Link to="/">About me</Link>
-          </Menu.Item>
-          <Menu.Item key="2" icon={<FontAwesomeIcon icon={faBook} />}>
-            <Link to="/experience">Experience</Link>
-          </Menu.Item>
-          <Menu.Item key="3" icon={<FontAwesomeIcon icon={faChess} />}>
-            <Link to="/skills">Skills</Link>
-          </Menu.Item>
-          <Menu.Item key="4" icon={<PhoneFilled />}>
-            <Link to="/contact">Contact</Link>
-          </Menu.Item>
-        </Menu>
+        <SideNavbarContent></SideNavbarContent>
       </Sider>
       <Layout className="container">
         <Switch>
@@ -59,6 +78,6 @@ const App = () => (
       </Layout>     
     </Layout>
   </Router>
-);
+)};
 
 export default App;

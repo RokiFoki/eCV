ef1afae62a311f435d833fa7211a42e053b820a8
Roko Krstulović <roko.krstulovic.opara@gmail.com>
2021-06-26 07:39:51 +0200

    expandable project

--- ClientApp/src/Projects/Projects.tsx
+++ ClientApp/src/Projects/Projects.tsx
@@ -1,4 +1,4 @@
-import { Card, List, Modal, Tooltip } from 'antd';
+import { Card, List, Modal, Tooltip, Typography } from 'antd';
 import React, { useState } from 'react';
 import styles from './Projects.module.scss';
 import droneImg from '../Content/drone-diagram.png';
@@ -16,8 +16,11 @@ const tech: {name: string, description: string}[] = [
   {name: 'Android/Java', description: 'Used to implement network measurement tests.'}, 
 ]
 
+const title = 'Self flying drone for network performance measurements';
+
 const Projects = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   const showModal = () => {
     setIsModalVisible(true);
@@ -29,37 +32,40 @@ const Projects = () => {
 
   return (
   <div className={styles.Projects}>
-    <Card title="Self flying drone for network performance measurements" bordered={false}>
-      <Tooltip placement="bottom" title="Click for full screen">
-        <img src={droneImg} className={styles.ProjectImage} alt="Drone Diagram" onClick={showModal}></img>
-      </Tooltip>
-      <section>
-        <p>The project involved a drone that is self-controlled, holding an Android device that measures network bandwidth. The position, status, and network bandwidth are displayed in real-time on a web application where users can send basic commands to the drone. The drone has sensors on it making it able to avoid obstacles on the given path.</p>
-        <p>I worked on the development of the AI for drone movement, real-time communication with the server, server with real-time information about the drone’s position and status. Assisted in developing measurement tests for the Android device that the drone was holding. </p>
-      </section>
-      
-      <section>
+    <Tooltip title={title}>
+      <Card title={title} 
+        className={`${styles.Project} ${!expanded ? styles.NotExpanded: ''}`} 
+        bordered={false} onClick={() => setExpanded(!expanded)}>
+        {expanded && <Tooltip placement="bottom" title="Click for full screen">
+          <img src={droneImg} className={styles.ProjectImage} alt="Drone Diagram" onClick={showModal}></img>
+        </Tooltip>}
+        <Typography>
+          <Typography.Paragraph ellipsis={{rows: 3, expandable: true, symbol: 'more'}}>The project involved a drone that is self-controlled, holding an Android device that measures network bandwidth. The position, status, and network bandwidth are displayed in real-time on a web application where users can send basic commands to the drone. The drone has sensors on it making it able to avoid obstacles on the given path.</Typography.Paragraph>
+          {expanded && <Typography.Paragraph>I worked on the development of the AI for drone movement, real-time communication with the server, server with real-time information about the drone’s position and status. Assisted in developing measurement tests for the Android device that the drone was holding. </Typography.Paragraph>}
+        </Typography>
         
-      </section>
-      <div className="full-width" style={{clear: 'both'}}>
-        <b>Technology:</b>
-        <ul>
-          <List
-            size="small"
-            dataSource={tech}
-            renderItem={item => 
-            <List.Item key={item.name}>
-              <List.Item.Meta title={item.name} description={item.description} />
-            </List.Item>}
-          />
-        </ul>  
-      </div>
-      <p><strong>Buzzwords:</strong> IOT, AI, ML</p>
-    </Card>
+        {expanded && <div className="full-width" style={{clear: 'both'}}>
+          <b>Technology:</b>
+          <ul>
+            <List
+              size="small"
+              dataSource={tech}
+              renderItem={item => 
+              <List.Item key={item.name}>
+                <List.Item.Meta title={item.name} description={item.description} />
+              </List.Item>}
+            />
+          </ul>  
+        </div>}
+        {!expanded && <p><strong>Technology:</strong> {tech.map(t => t.name).join(', ')}</p>}
+        <p><strong>Buzzwords:</strong> IOT, AI, ML</p>
+      </Card>
+    </Tooltip>
     <Modal title={null} visible={isModalVisible} footer={null} onCancel={hideModal} width={650+50}>
-      <img src={droneImg} style={{maxWidth: '650px', marginTop: '30px'}} alt="Drone Diagram"></img>
+      <div style={{overflow: 'auto'}}>
+        <img src={droneImg} style={{maxWidth: '650px', marginTop: '30px'}} alt="Drone Diagram"></img>
+      </div>
     </Modal>
-
   </div>
 )};
 

d377bcc2ed4f4d89dc4ba615481979aa8aad66b9
Roko Krstulović <roko.krstulovic.opara@gmail.com>
2021-07-10 09:50:19 +0200

    fix typo

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -35,7 +35,7 @@ const SideNavbarContent = withRouter((props: SideNavbarContentProps) => {
   }
   return (
     <React.Fragment>
-      <div className="logo">ePortofolio</div>
+      <div className="logo">ePortfolio</div>
       <Menu theme="dark" mode="inline" selectedKeys={[selectionKey]}>
         <Menu.Item key="1" icon={<UserOutlined />} onClick={props.onClick}>
           <Link to="/">About me</Link>

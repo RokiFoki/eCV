4f33f74790765db234757ab3200fcf76090eb23b
Roko Krstulović <roko.krstulovic.opara@gmail.com>
2021-07-10 07:46:44 +0200

    add progresbar to skilltree

--- ClientApp/src/Skills/SkillTree/SkillTree.tsx
+++ ClientApp/src/Skills/SkillTree/SkillTree.tsx
@@ -1,17 +1,32 @@
-import { Tree } from 'antd';
+import { Progress, Tree } from 'antd';
 import React, { useState } from 'react';
 import styles from './SkillTree.module.scss';
 import skills, { ISkill } from '../../Shared/skills-data';
 
 interface ITreeData {
-  title: string;
+  title: string | JSX.Element;
   key: string; 
   children: ITreeData[];
 }
 
+const orange = 'rgb(255, 153, 51)';
+const yellow = 'hsl(60, 100%, 50%)';
+const lightGreen = 'rgb(204, 255, 51)';
+const green = 'rgb(51, 204, 51)';
+
 function toTreeNode(skill: ISkill): ITreeData {
   return {
-    title: skill.name,
+    title: (
+    <>
+      <div>{skill.name}</div>
+      <div style={{width: 80, margin: '-8px 0 -5px 0'}}>
+              <Progress percent={skill.experience} showInfo={false} 
+                strokeColor={ skill.experience >= 90 ? green :
+                              skill.experience >= 75 ? lightGreen :
+                              skill.experience >= 50 ? yellow : orange } 
+                strokeWidth={5}/>
+              </div>
+    </>),
     key: skill.key,
     children: skill.children.map(c => toTreeNode(c))
   }

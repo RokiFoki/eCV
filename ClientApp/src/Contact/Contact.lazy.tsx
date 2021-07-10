813dde96c9f1d351aa62277acc89f41a53741634
Roko Krstulović <roko.krstulovic.opara@gmail.com>
2021-07-10 09:21:34 +0200

    better skeleton for contact

--- ClientApp/src/Contact/Contact.lazy.tsx
+++ ClientApp/src/Contact/Contact.lazy.tsx
@@ -3,8 +3,18 @@ import React, { lazy, Suspense } from 'react';
 
 const LazyContact = lazy(() => import('./Contact'));
 
+const fallback = (
+  <>
+    <div style={{margin: 10}}><Skeleton active/></div>
+    <div style={{margin: 10}}><Skeleton active/></div>
+    <div style={{margin: 10}}><Skeleton active/></div>
+    <div style={{margin: 10}}><Skeleton active/></div>
+    <div style={{margin: 10}}><Skeleton active/></div>
+  </>
+);
+
 const Contact = (props: JSX.IntrinsicAttributes & { children?: React.ReactNode; }) => (
-  <Suspense fallback={<Skeleton active />}>
+  <Suspense fallback={fallback}>
     <LazyContact {...props} />
   </Suspense>
 );

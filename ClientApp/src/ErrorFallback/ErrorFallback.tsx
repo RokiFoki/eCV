ba06a8a2d8595ca2741cef431047b9607317e6cb
Roko Krstulović <roko.krstulovic.opara@gmail.com>
2021-09-17 18:31:03 +0200

    errorboundary: reload on issue

--- ClientApp/src/ErrorFallback/ErrorFallback.tsx
+++ ClientApp/src/ErrorFallback/ErrorFallback.tsx
@@ -4,10 +4,10 @@ import React from 'react';
 const ErrorFallback : React.FC<any> = ({error, resetErrorBoundary}: any) => (
     <div style={{width:'100%', height:'100%', display: 'flex', justifyContent:'center', alignItems:'center'}}>
       <div style={{display: 'flex', flexFlow: 'column', alignItems: 'center'}}>
-        <h1 style={{textAlign: 'center'}}>Something went wrong. Please try again or select another option in sidebar.</h1>
+        <h1 style={{textAlign: 'center'}}>Something went wrong. Please reload the page.</h1>
         <div>
-          <Button type="primary" size="large" onClick={resetErrorBoundary}>
-            Try again
+          <Button type="primary" size="large" onClick={() => window.location.reload()}>
+            Reload
           </Button>
         </div>
       </div>
